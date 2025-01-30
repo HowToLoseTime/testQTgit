0798b51933cb97270c09b5f35aef61a49579270a
HowToLoseTime <tikshaevr@bk.ru>
2025-01-30 16:54:03 +0300

    test for git

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
-#include<iostream>
+#include "mainwindow.h"
 
-int main(){
-  ;
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
